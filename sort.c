b82bf675fc7a1fea3882fcc7ba6e73385062dee6
Ladapa2510 <106828161+Ladapa2510@users.noreply.github.com>
2025-02-01 21:25:33 +0700

    Add files via upload

--- sort.c
+++ sort.c
@@ -1,78 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ludomsak <ludomsak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 16:45:59 by ludomsak          #+#    #+#             */
+/*   Updated: 2025/02/01 21:21:08 by ludomsak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-void    sort_two(t_stack *stack_a)
+void	two_sort(t_stack **stack_a)
 {
-    if (stack_a->number > stack_a->next->number)
-        sa(stack_a);
+	if ((*stack_a)->number > (*stack_a)->next->number)
+		sa(stack_a);
 }
 
-void    sort_three(t_stack *stack_a)
+void	three_sort(t_stack **stack_a)
 {
-    if ((stack_a->number > stack_a->next->number) && (stack_a->number > stack_a->next->next->number))
-        ra(stack_a);
-    if ((stack_a->next->next->number > stack_a->next->number) && stack_a->next->next->number > stack_a->next->next->next->number)
-        rra(stack_a);
-    if (stack_a->next->number > stack_a->next->next->number)
-        sa(stack_a);
-}
-void    sort_five(t_stack *stack_a, t_stack *stack_b)
-{
-    int min;
+	int	a;
+	int	b;
+	int	c;
 
-    min = stack_a->index - 3;
-    while (stack_a->index > 3)
-    {
-        if (stack_a->number == min || stack_a->number == min + 1)
-            pb(stack_a, stack_b);
-       else if (stack_a->number == min + 2 || stack_a->number == min + 3)
-       {
-           pb(stack_a, stack_b);
-           ra(stack_a);
-       }
-    }
-    sort_three(stack_a);
-    while (stack_b && stack_b->index > 0)
-        pa(stack_a, stack_b);
+	a = (*stack_a)->number;
+	b = (*stack_a)->next->number;
+	c = (*stack_a)->next->next->number;
+	if (a > b && b > c)
+		sa(stack_a);
+	else if (a > b && b < c && a < c)
+		ra(stack_a);
+	else if (a > b && b < c && a > c)
+		rra(stack_a);
+	else if (a < b && a > c)
+	{
+		sa(stack_a);
+		ra(stack_a);
+	}
 }
 
-void    sort_hundred(t_stack *stack_a, t_stack *stack_b)
+void	five_sort(t_stack **stack_a, t_stack **stack_b)
 {
-    int size;
-
-    size = stack_a->index;
-    while (stack_a->index > 3 && stack_a->next->number != size)
-    {
-        if (stack_a->next->number < size / 2)
-            ra(stack_a);
-        else
-            rra(stack_a);
-    }
-    pb(stack_a, stack_b);
-    sort_three(stack_a);
-    while (stack_b->index > 0)
-        pa(stack_a, stack_b);
+	if ((*stack_a)->number > (*stack_a)->next->number)
+		sa(stack_a);
+	else if ((*stack_a)->number < (*stack_a)->next->number 
+		&& (*stack_a)->next->number < (*stack_a)->next->next->number)
+		pb(stack_a, stack_b);
+	three_sort(stack_a);
+	if ((*stack_b)->number < (*stack_b)->next->number)
+		pa(stack_b, stack_a);
 }
-void    radix_sort(t_stack *stack_a, t_stack *stack_b)
-{
-    int max;
-    int i;
-    int j;
 
-    max = stack_a->index;
-    i = 0;
-    j = stack_a->index;
-    while (i < max)
-    {
-        while (j < 0)
-        {
-            if (((stack_a->number >> i) & 1) == 1)
-                ra(stack_a);
-            else
-                pb(stack_a, stack_b);
-        }
-        while (stack_b->index > 0)
-            pa(stack_a, stack_b);
-        i++;
-    }
- }
- 
+int	main()
+{
+	t_stack *a;
+	t_stack	*b;
+
+	a = NULL;
+	b = NULL;
+	stack_addback(&a, stack_new(8));
+	stack_addback(&a, stack_new(3));
+	stack_addback(&a, stack_new(2));
+	stack_addback(&a, stack_new(4));
+	stack_addback(&a, stack_new(1));
+	five_sort(&a, &b);
+	return (0);
+}
